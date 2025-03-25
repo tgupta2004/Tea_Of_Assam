f0cfd56103735356e9749404112b14b5eede4a02
tgupta2004 <nidhideepjyoti@student.sfit.ac.in>
2025-03-25 12:06:55 +0530

    Updates

---
+++ sw.js
@@ -0,0 +1,8 @@
+self.addEventListener('install', (event) => {
+    console.log('Service Worker installing...');
+    self.skipWaiting();
+  });
+  
+  self.addEventListener('activate', (event) => {
+    console.log('Service Worker activating...');
+  });
